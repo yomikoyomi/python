f5a0c1f257b62185b42521d0bc10c787847a1e03
yomikoyomi <yomi.koyomi@gmail.com>
2015-11-07 23:55:39 +0900

    python3

--- dic.py
+++ dic.py
@@ -3,11 +3,10 @@
 dict_1 = {"YEAR": "2015", "MONTH": "07", "DAY": "05"}
 
 if __name__ == "__main__":
-    print dict_1
+    print(dict_1)
 
     for key in dict_1:
-        print key + " as ",
-        print dict_1.get(key)
-    print dict_1.has_key("YEAR")
-    print "DAYS" in dict_1
-    print "test"
+        print(key + " as ",)
+        print(dict_1.get(key))
+    print('YEAR' in dict_1)
+    print("DAYS" in dict_1)
